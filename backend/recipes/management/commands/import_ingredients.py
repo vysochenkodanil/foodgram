0f4bbb56345aae6f8831c95a46f99ec7fb0ea2fd
VysochenkoDanil <czzzrcoooll@inbox.ru>
2025-06-11 19:27:11 +0300

    загрузка ингридиентов через програмы

--- backend/recipes/management/commands/import_ingredients.py
+++ backend/recipes/management/commands/import_ingredients.py
@@ -1,14 +1,16 @@
 import json
+
 from django.core.management.base import BaseCommand
+
 from recipes.models import Ingredient
+
 
 class Command(BaseCommand):
     help = 'Загрузка ингредиентов из JSON'
 
     def handle(self, *args, **options):
-        # Путь к файлу (можно изменить под свою структуру)
         file_path = 'backend/data/ingredients.json'
-        
+
         with open(file_path, 'r', encoding='utf-8') as f:
             data = json.load(f)
             for item in data:
@@ -16,5 +18,5 @@ class Command(BaseCommand):
                     name=item['name'],
                     measurement_unit=item['measurement_unit']
                 )
-        
+
         self.stdout.write('Данные успешно загружены')
