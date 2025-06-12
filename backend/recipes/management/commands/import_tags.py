06b93e3bf9e043271e62d965cdc39e4b3ec7c8c1
VysochenkoDanil <czzzrcoooll@inbox.ru>
2025-06-12 16:22:12 +0300

    добавление тэг

--- backend/recipes/management/commands/import_tags.py
+++ backend/recipes/management/commands/import_tags.py
@@ -1,4 +1,4 @@
-import csv 
+import csv
 
 from django.core.management.base import BaseCommand
 from recipes.models import Tag
@@ -8,14 +8,14 @@ class Command(BaseCommand):
     help = "Загружаем тэги из csv."
 
     def handle(self, *args, **options):
-        with open(
-            "data/tags.csv", encoding="utf-8"
-            ) as file:
+        with open("data/tags.csv", encoding="utf-8") as file:
             reader = csv.reader(file)
             for row in reader:
                 name, slug = row
-                obj, created = Tag.objects.get_or_create(
-                    name=name.strip(), slug=slug.strip()
+                Tag.objects.get_or_create(
+                    name=name.strip(),
+                    slug=slug.strip()
                 )
-        self.stdout.write(self.style.SUCCESS(
-            f"Теги загружены, мой господин."))
+        self.stdout.write(
+            self.style.SUCCESS("Теги загружены, мой господин.")
+        )
