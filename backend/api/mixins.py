0e46bf85d3fcb2fb1a387bb6aa1147ef2b13439c
VysochenkoDanil <czzzrcoooll@inbox.ru>
2025-06-16 16:06:31 +0300

    в миксине вынес в отдельную логику метод .exists()

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -3,11 +3,6 @@ from rest_framework.response import Response
 
 
 class RecipeActionMixin:
-    model = None
-    serializer_class = None
-    error_message = ""
-    success_status = status.HTTP_201_CREATED
-
     def perform_action(
         self, request, pk, model, serializer_class, error_message
     ):
@@ -16,29 +11,27 @@ class RecipeActionMixin:
         queryset = model.objects.filter(user=user, recipe=recipe)
         exists = queryset.exists()
 
-        if (request.method == "POST" and exists) or (
-            request.method == "DELETE" and not exists
-        ):
-            msg = (
-                error_message
-                if request.method == "POST"
-                else f"Рецепт не найден в {model._meta.verbose_name}."
-            )
-            return Response(
-                {"errors": msg},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
         if request.method == "POST":
+            if exists:
+                return Response(
+                    {"errors": error_message},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
             model.objects.create(user=user, recipe=recipe)
             serializer = serializer_class(
-                recipe,
-                context={"request": request}
+                recipe, context={"request": request}
             )
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        queryset.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        elif request.method == "DELETE":
+            if not exists:
+                return Response(
+                    {
+                        "errors": (
+                            f"Рецепт не найден в {model._meta.verbose_name}."
+                        )
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            queryset.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
